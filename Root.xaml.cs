f82568d4f1ef824b778d2a6ef1713b9ea456cf18
KubaI2009 <kuba.izydorczyk09@gmail.com>
2025-07-21 13:23:55 +0200

    widgets added

--- Root.xaml.cs
+++ Root.xaml.cs
@@ -14,9 +14,9 @@ namespace RandomMazeGame;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class Root : Window
 {
-    public MainWindow()
+    public Root()
     {
         InitializeComponent();
     }
